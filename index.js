1509f47ad36fd964c7c05f891e4dccf2d258d129
ytrms <lorenzo.gravina@me.com>
2021-11-28 18:31:34 +0300

    complete exercise 3.6

--- index.js
+++ index.js
@@ -52,6 +52,35 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000)
+}
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number missing."
+    })
+  }
+
+  if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "Name already present."
+    })
+  }
+
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id": generateId()
+  }
+
+  persons = persons.concat(person)
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
