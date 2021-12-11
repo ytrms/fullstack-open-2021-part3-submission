81d53d0bb8e69fabb6aa02df12e10e2d5f492656
ytrms <lorenzo.gravina@me.com>
2021-12-11 11:32:20 +0300

    complete exercise 3.9

--- index.js
+++ index.js
@@ -1,93 +1,118 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
-    "number": "040-123456"
+    "number": "040-123456",
   },
   {
     "id": 2,
     "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    "number": "39-44-5323523",
   },
   {
     "id": 3,
     "name": "Dan Abramov",
-    "number": "12-43-234345"
+    "number": "12-43-234345",
   },
   {
     "id": 4,
     "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+    "number": "39-23-6423122",
+  },
+];
 
-const app = express()
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqbody",
+  ),
+);
 
-morgan.token('reqbody', (request, response) => {
-  return JSON.stringify(request.body)
-})
+morgan.token("reqbody", (request, response) => {
+  return JSON.stringify(request.body);
+});
 
-app.get('/api/persons', (request, response) => {
-  response.send(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
 
-app.get('/info', (request, response) => {
-  response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`)
-})
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`,
+  );
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
   if (person) {
-    response.send(person)
+    response.send(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+// app.put("/api/persons/:id", (request, response) => {
+//   const body = request.body;
+//   const id = Number(request.params.id);
+//   persons = persons.filter((person) => person.id !== id)
+//   const newPerson = {
+//     name: body.name,
+//     number: body.number,
+//     id: id
+//   }
+//   persons = persons.concat(newPerson);
+//   response.send(newPerson);
+//   response.status(204).end();
+// })
 
-  response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
 
 const generateId = () => {
-  return Math.floor(Math.random() * 1000000000)
-}
+  return Math.floor(Math.random() * 1000000000);
+};
 
-app.post('/api/persons/', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "Name or number missing."
-    })
+      error: "Name or number missing.",
+    });
   }
 
-  if (persons.map(person => person.name).includes(body.name)) {
+  if (persons.map((person) => person.name).includes(body.name)) {
     return response.status(400).json({
-      error: "Name already present."
-    })
+      error: "Name already present.",
+    });
   }
 
   const person = {
     "name": body.name,
     "number": body.number,
-    "id": generateId()
-  }
+    "id": generateId(),
+  };
 
-  persons = persons.concat(person)
-  response.status(204).end()
-})
+  console.log(person)
 
-const PORT = 3001
+  persons = persons.concat(person);
+  response.send(person);
+  response.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
